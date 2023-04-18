dc239245596ac1b4cf960d7d2249aa39dafc6bad
piyush-1871 <piyush1871b@gmail.com>
2023-04-18 14:41:58 +0530

    done

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import CartItem from "../components/CartItem";
+
+const Cart = () => {
+  const { cart } = useSelector((state) => state);
+  console.log("Printing Cart");
+  console.log(cart);
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect(() => {
+    setTotalAmount(cart.reduce((acc, curr) => acc + curr.price, 0));
+  }, [cart]);
+
+  return (
+    <div>
+      {cart.length > 0 ? (
+        <div>
+          <div>
+            {cart.map((item, index) => {
+              return <CartItem key={item.id} item={item} itemIndex={index} />;
+            })}
+          </div>
+
+          <div>
+            <div className="flex flex-col gap-5 ">
+              <div className="font-semibold text-xl text-green-800 ">
+                Your Cart
+              </div>
+              <div className="font-semibold text-5xl text-green-700  -mt-5">
+                Summary
+              </div>
+              <p className="text-xl">
+                <span className="text-gray-700 font-semibold text-xl">
+                  Total Items: {cart.length}
+                </span>
+              </p>
+            </div>
+
+            <div className="flex flex-col">
+              <p className="text-xl font-bold">
+                <span className="text-gray-700 font-semibold">Total Amount: </span>${totalAmount}
+              </p>
+              <button className="bg-green-700 hover:bg-purple-50 rounded-lg text-white transition duration-300 ease-linear mt-5 border-2 border-green-600 font-bold hover:text-green-700 p-3 text-xl">CheckOut Now</button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1>Cart Empty</h1>
+          <Link to={"/"}>
+            <button>Shop Now</button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
